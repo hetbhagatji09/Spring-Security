9955247f110f263bfcba47420079cab081d1846a
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-12 23:11:11 +0530

    Use BcryptPasswordEncoder for storing password in encoded form

--- src/main/java/com/example/SpringSecurityDemo/service/UserService.java
+++ src/main/java/com/example/SpringSecurityDemo/service/UserService.java
@@ -3,11 +3,14 @@ package com.example.SpringSecurityDemo.service;
 import com.example.SpringSecurityDemo.Dao.UserRepo;
 import com.example.SpringSecurityDemo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserService {
+
+    BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(10);
 
     @Autowired
     private UserRepo userRepo;
@@ -15,6 +18,7 @@ public class UserService {
 
 
     public User saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 }
