39cbcf792e0bd71258e39b513c78e180573d1c9e
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-12 22:05:28 +0530

    created our own authentication provider

--- src/main/java/com/example/SpringSecurityDemo/Config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/Config/SecurityConfig.java
@@ -1,21 +1,24 @@
 package com.example.SpringSecurityDemo.Config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     //if we want to change in configuration we have to return securityfilterchain object
     //it is our configuration
@@ -25,7 +28,8 @@ public class SecurityConfig {
         http.csrf(customizor->customizor.disable());
         //unable sign up authentication page
         http.authorizeHttpRequests(request->request.anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults()).
+                logout(Customizer.withDefaults())
         //make session stateless
         //every time you hit the url or make a new request it gives you enew session id
         .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -34,18 +38,25 @@ public class SecurityConfig {
         return http.build();
 
     }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user1= User.withDefaultPasswordEncoder().
+//                username("harsh").
+//                password("1234").
+//                roles("USER").
+//                build();
+//        UserDetails user2= User.withDefaultPasswordEncoder().
+//                username("shruti").
+//                password("1234").
+//                roles("ADMIN").
+//                build();
+//        return new InMemoryUserDetailsManager(user1);
+//    }
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user1= User.withDefaultPasswordEncoder().
-                username("harsh").
-                password("1234").
-                roles("USER").
-                build();
-        UserDetails user2= User.withDefaultPasswordEncoder().
-                username("shruti").
-                password("1234").
-                roles("ADMIN").
-                build();
-        return new InMemoryUserDetailsManager(user1);
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
     }
 }
