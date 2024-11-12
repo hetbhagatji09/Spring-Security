8b460a2c729421a96d4a9781382ae64c177cd96b
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-12 15:21:23 +0530

    create student class as spring component

---
+++ src/main/java/com/example/SpringSecurityDemo/Student.java
@@ -0,0 +1,18 @@
+package com.example.SpringSecurityDemo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int roll;
+    private String name;
+    private String tech;
+
+}
