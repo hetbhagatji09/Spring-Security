400d651b12cd542987f68dbda1626260f306a3ec
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-02 01:32:45 +0530

    generating id using hibernate

--- src/main/java/com/example/SpringSecurityDemo/model/User.java
+++ src/main/java/com/example/SpringSecurityDemo/model/User.java
@@ -2,9 +2,12 @@ package com.example.SpringSecurityDemo.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -13,6 +16,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
     @Id
+    //hibernate is genarting id for you by generatedvalue annotation
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
