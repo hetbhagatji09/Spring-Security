79c5c57b65d0f3c54d6259c1cae9fe1b1a123519
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-02 10:03:05 +0530

    created login api to authenticate user and for login

--- src/main/java/com/example/SpringSecurityDemo/controller/UserController.java
+++ src/main/java/com/example/SpringSecurityDemo/controller/UserController.java
@@ -2,8 +2,12 @@ package com.example.SpringSecurityDemo.controller;
 
 
 import com.example.SpringSecurityDemo.model.User;
+import com.example.SpringSecurityDemo.service.JWTService;
 import com.example.SpringSecurityDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @Autowired
+    private JWTService jwtService;
+    @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("register")
     public User registerUser(@RequestBody User user){
         return userService.saveUser(user);
     }
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+
+        Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+        if(authentication.isAuthenticated()){
+            return jwtService.generateToken(user.getUsername());
+        }
+        else{
+            return "Login Failed";
+        }
+
+    }
+
 }
