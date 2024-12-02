b062c4ed4cc980a3382539a098867f6739f32921
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-02 10:00:50 +0530

    Add bean for authentication manager for login api

--- src/main/java/com/example/SpringSecurityDemo/Config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/Config/SecurityConfig.java
@@ -4,9 +4,11 @@ package com.example.SpringSecurityDemo.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,7 +30,8 @@ public class SecurityConfig {
 
         http.csrf(customizor->customizor.disable());
         //unable sign up authentication page
-        http.authorizeHttpRequests(request->request.requestMatchers("register").permitAll().anyRequest().authenticated())
+        // configure security to allow public access to the /register endpoint
+        http.authorizeHttpRequests(request->request.requestMatchers("register","login").permitAll().anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
         //make session stateless
         //every time you hit the url or make a new request it gives you enew session id
@@ -58,5 +61,10 @@ public class SecurityConfig {
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(10));
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
